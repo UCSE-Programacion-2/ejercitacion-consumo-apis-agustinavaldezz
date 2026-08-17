218b056aeaa51cc6334acd36a701aad46a35bb85
agustinavaldezz <agustina.valdez1809@gmail.com>
2026-08-16 23:41:57 -0300

    feat(js): renderizar foto nombre e email en el dom
    Closes #3

--- js/index.js
+++ js/index.js
@@ -18,8 +18,22 @@
 //    Estos elementos ya tienen estilos definidos en css/styles.css
 //    (.tarjeta img, .tarjeta h2, .tarjeta p).
 
+async function obtenerUsuario() {
+  const response = await fetch('http://localhost:3000/api/user');
+  const datos = await response.json();
+  renderizarDatosUsuario(datos);
+}
+
+obtenerUsuario();
 function renderizarDatosUsuario(datos) {
-  // Escribe aquí tu código para mostrar la foto, nombre completo e email en div.tarjeta
+  const usuario = datos.results[0];
+  const tarjeta = document.querySelector('.tarjeta');
+
+  tarjeta.innerHTML = `
+    <img src="${usuario.picture.large}" alt="Foto de perfil">
+    <h2>${usuario.name.title} ${usuario.name.first} ${usuario.name.last}</h2>
+    <p>${usuario.email}</p>
+  `;
 }
 
 /* -------------------------------- CONSIGNA 2 -------------------------------- */
